8c3ff2b0dd7f26a7dc1eebe2bc346c0f308029d3
MTRNord <mtrnord1@gmail.com>
2023-10-05 11:22:00 +0200

    Update and add some more tests

--- __test__/index.spec.mjs
+++ __test__/index.spec.mjs
@@ -1,8 +1,10 @@
 import test from 'ava'
-
+import { dirname, join } from 'path';
+import { fileURLToPath } from 'url';
 import { YaraCompiler } from '../index.js'
 
 const TEST_RULE = "rule TestRule {\n    condition:\n        true\n}"
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 test('can construct YaraCompiler', (t) => {
   t.plan(1)
@@ -20,7 +22,6 @@ test('can load string rules', (t) => {
   });
 })
 
-
 test('can match string rules', (t) => {
   t.plan(2)
   t.notThrows(() => {
@@ -36,6 +37,70 @@ test('can match string rules', (t) => {
         metadatas: [],
         tags: [],
         strings: []
+      }
+    ])
+  });
+})
+
+test('can load file based rules', (t) => {
+  t.plan(1)
+  t.notThrows(() => {
+    const compiler = new YaraCompiler([{
+      filename: join(__dirname, "./test.yara")
+    }], []);
+  });
+})
+
+
+test('can match file based rules', (t) => {
+  t.plan(2)
+  t.notThrows(() => {
+    const compiler = new YaraCompiler([{
+      filename: join(__dirname, "./test.yara")
+    }], []);
+    const scanner = compiler.newScanner();
+    const result = scanner.scanString("Test");
+    t.deepEqual(result, [
+      {
+        identifier: "TestRule",
+        namespace: "default",
+        metadatas: [
+          {
+            identifier: 'Author',
+            value: 'MTRNord',
+          },
+          {
+            identifier: 'Description',
+            value: 'Test Rule',
+          },
+          {
+            identifier: 'hash',
+            value: '06fdc3d7d60da6b884fd69d7d1fd3c824ec417b2b7cdd40a7bb8c9fb72fb655b',
+          },
+          {
+            identifier: 'Action',
+            value: 'Notify',
+          },
+        ],
+        tags: ["test_rule"],
+        strings: [
+          {
+            identifier: '$test_string',
+            matches: [
+              {
+                base: 0,
+                data: [
+                  84,
+                  101,
+                  115,
+                  116
+                ],
+                length: 4,
+                offset: 0,
+              },
+            ],
+          },
+        ]
       }
     ])
   });
---
+++ __test__/test.yara
@@ -0,0 +1,13 @@
+rule TestRule : test_rule
+{
+    meta:
+        Author = "MTRNord"
+        Description = "Test Rule"
+        hash = "06fdc3d7d60da6b884fd69d7d1fd3c824ec417b2b7cdd40a7bb8c9fb72fb655b"
+        Action = "Notify"
+    strings:
+        $test_string = "Test" ascii nocase
+
+    condition:
+        $test_string
+}
